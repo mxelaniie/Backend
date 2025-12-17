6f4a9e1cfa8d1bcc1ed454246a63e143f1f836d2
mxelaniie <melanie.schiefermueller@students.fhnw.ch>
2025-12-17 14:14:24 +0100

    Origins Vercel hinzufügen

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ app = FastAPI()
 origins = [
     "http://localhost:5173",
     "http://localhost:5174", # falls zwei laufen
+    "https://backend-rouge-gamma-83.vercel.app/" # für vercel Deployment
 ]
 
 app.add_middleware(
